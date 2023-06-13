4a247747e1acf1b0b49523e6fd34ccf7eb761d4e
Sachin Munda <sachinmunda954@gmail.com>
2023-06-13 19:32:51 +0530

    simple QRCode-Gen

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import QRcode from 'qrcode'
+import {useState} from 'react'
+
+function App() {
+  const [url, setUrl] = useState('')
+  const[qrcode, setQrcode] = useState('')
+
+  const GenerateQRCode = () => {
+    QRcode.toDataURL(url, {
+      width: 800,
+      margin: 2,
+      color: {
+        dark: '#000000ff',
+        light: 'ffffffff'
+      }
+    },(err, url) => {
+      if(err){
+       return  console.error(err)
+      }
+     // console.log(url)
+      setQrcode(url)
+    })
+  }
+
+
+  return (
+    <>
+      <div classname="app">
+        <h1>QR Code Generator</h1>
+        <input
+          type="text"
+          placeholder="e.g. https://google.com"
+          value={url}
+          onChange={(evt)=> setUrl(evt.target.value)} />
+
+        <button onClick={GenerateQRCode}>Generate</button> 
+        { qrcode && <>
+          <img src={qrcode} alt="" />
+          <a href={qrcode} download="qrcode.png">Download</a>
+        </>}
+
+      </div>
+    </>
+  )
+}
+
+export default App
